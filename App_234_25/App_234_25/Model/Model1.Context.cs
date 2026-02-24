ff1b233929fe9d684340e22abf4e29a35330c07f
TolyaKPyT250 <saahmetovanatolij85@gmail.com>
2026-02-24 20:19:38 +0500

    240226(3)
    -Переподключить БД!!!
    -Презентация

--- App_234_25/App_234_25/Model/Model1.Context.cs
+++ App_234_25/App_234_25/Model/Model1.Context.cs
@@ -29,7 +29,6 @@ namespace App_234_25.Model
         public virtual DbSet<Lecturer> Lecturers { get; set; }
         public virtual DbSet<Room> Rooms { get; set; }
         public virtual DbSet<Schedule> Schedules { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<User> Users { get; set; }
     }
 }
